d74e13335fb25f23403d4f758684e9326564c67a
Maria Rodrigues <mlnr@cin.ufpe.br>
2017-04-02 22:59:28 -0300

    Arquivos redirecionados

--- src/IDateAndTime.java
+++
@@ -1,9 +0,0 @@
-
-public interface IDateAndTime {
-	
-	//parameter can be: 'hour', 'min' or 'sec'
-	int time (String p1);
-	
-	//parameter can be: 'day', 'mon' or 'year'
-	String date (String p1);
-}
